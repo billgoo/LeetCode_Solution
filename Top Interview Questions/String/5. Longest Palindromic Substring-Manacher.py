2c15e3a7fd37b8a1898e20728d9b2f603e6b8ea0
billgoo <billgoo0813@gmail.com>
2019-12-29 12:04:01 +0800

    2019.12.29

---
+++ Top Interview Questions/String/5. Longest Palindromic Substring-Manacher.py
@@ -0,0 +1,94 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+        
+        # dp: p[i,j] = (p[i+1, j-1] and si==sj)
+        # expand 1, 2, 3, n
+        # time n^2, space n^2
+        '''
+        n = len(s)
+        p = [[0 for i in range(n)] for j in range(n)]
+        p[0][0] = 1
+        for i in range(1, n):
+            p[i][i] = 1
+            if s[i-1] == s[i]:
+                p[i-1][i] = 1
+        
+        for j in range(n):
+            for i in range(j):
+                if p[i+1][j-1] and (s[i]==s[j]):
+                    p[i][j] = 1
+        
+        result = ""
+        m= 0
+        for i in range(n):
+            for j in range(i, n):
+                if j-i+1 > m and p[i][j]:
+                    m = j-i+1
+                    result = s[i:j+1]
+                    
+        return result
+        '''
+        # expand from center and have 2n-1 centers
+        # time n^2, space n
+        '''
+        start, end = 0, 0
+        
+        n = len(s)
+        for i in range(n):
+            # center at one num
+            l = r = i
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1; r += 1
+            len1 = r - l - 1
+            
+            # center between 2 num
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1; r += 1
+            len2 = r - l - 1
+            
+            max_len = max(len1, len2)
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+        
+        return s[start:end+1]
+        '''
+        
+        # Manacher's Algorithm
+        # time n, space n
+        def add_boundary(s):
+            new_s = "#"
+            for c in s:
+                new_s += c + "#"
+            return new_s
+        
+        new_s = add_boundary(s)
+        n = len(new_s)
+        p = [0 for _ in range(n)]
+        max_right, center = 0, 0
+        max_len, start = 1, 0
+        
+        for i in range(n):
+            if i < max_right:
+                mirror = 2 * center - i
+                p[i] = min(max_right - i, p[mirror])
+            
+            l, r = i - (p[i] + 1), i + (p[i] + 1)
+            while l >= 0 and r < n and new_s[l] == new_s[r]:
+                l -= 1; r += 1
+                p[i] += 1
+                
+            if i + p[i] > max_right:
+                max_right = i + p[i]
+                center = i
+            
+            if max_len < p[i]:
+                max_len = p[i]
+                start = (i - max_len) // 2
+                
+        return s[start: start + max_len]
+        
+        
