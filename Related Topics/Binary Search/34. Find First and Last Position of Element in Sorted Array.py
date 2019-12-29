a921f5ae64eb582b5298202d05d33cd2b90d6271
billgoo <billgoo0813@gmail.com>
2019-12-29 20:12:37 +0800

    2019.12.29

---
+++ Related Topics/Binary Search/34. Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        l, r = 0, len(nums)
+        
+        # find left bound
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] >= target:
+                r = m
+            else:
+                l = m + 1
+        lo = l
+        
+        if lo == len(nums) or nums[lo] != target:
+            return [-1, -1]
+        
+        # find right bound
+        l, r = 0, len(nums)
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m
+            else:
+                l = m + 1
+        
+        return [lo, l - 1]
+            
