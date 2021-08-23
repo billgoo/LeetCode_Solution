5d6ceb3db57c6f5925b9f5ad7543e79cb43578f8
顾岩 <guyan.billy@bytedance.com>
2021-08-23 18:52:22 +0800

    feat: 动态规划

--- Microsoft/Array/509. Fibonacci Number.py
+++ Microsoft/Array/509. Fibonacci Number.py
@@ -1,10 +1,22 @@
+# 509.Fibonacci Number
+# @lc app=leetcode id=509 lang=python3
+#
+# [509] Fibonacci Number
+#
+
+# @lc code=start
 class Solution:
-    def fib(self, N: int) -> int:
-        F = [0, 1, 1]
-        for i in range(3, N+1):
-            F[i%3] = F[(i-2)%3] + F[(i-1)%3]
-            
-        return F[N%3]
+    def fib(self, n: int) -> int:
+        if n < 1:
+            return 0
+        if n == 2 or n == 1:
+            return 1
+        prev, curr = 1, 1
+        for i in range(3, n + 1):
+            s = prev + curr
+            prev = curr
+            curr = s
+        return curr
         
 """
 matrix solution
