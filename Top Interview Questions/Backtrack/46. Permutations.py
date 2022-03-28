d2c5539343b2eec70776df3cd32682e56a878215
billgoo <billgoo0813@gmail.com>
2022-03-28 17:42:45 +0800

    feat: update backtrack:labuladong/1/5

--- Top Interview Questions/Backtrack/46. Permutations.py
+++ Top Interview Questions/Backtrack/46. Permutations.py
@@ -9,21 +9,37 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         self.res = []
-        track = []
-
-        def backtrack(nums, track):
-            n = len(nums)
-            if len(track) == n:
-                self.res.append(track[:])
-                return
-
-            for num in nums:
-                if not num in track:
-                    track.append(num)
-                    backtrack(nums, track)
-                    track.pop()
-        backtrack(nums, track)
+        self.track = []
+        self.backtrack([], nums)
         return self.res
+        
+    def backtrack(self, path: List[int], nums: List[int]) -> None:
+        if len(nums) == 0:
+            self.res.append(path[:])
+            return
+        for i, x in enumerate(nums):
+            path.append(x)
+            self.backtrack(path[:], nums[:i] + nums[i+1:])
+            path.pop()
+        return
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         self.res = []
+#         track = []
+
+#         def backtrack(nums, track):
+#             n = len(nums)
+#             if len(track) == n:
+#                 self.res.append(track[:])
+#                 return
+
+#             for num in nums:
+#                 if not num in track:
+#                     track.append(num)
+#                     backtrack(nums, track)
+#                     track.pop()
+#         backtrack(nums, track)
+#         return self.res
 # @lc code=end
 # 2
 class Solution:
@@ -35,11 +51,11 @@ class Solution:
             for i in range(first, n):
                 nums[first], nums[i] = nums[i], nums[first]
                 backtrack(first + 1)
-                
+
                 nums[first], nums[i] = nums[i], nums[first]
-        
+
         n = len(nums)
         re = []
         backtrack(0)
-        
+
         return re
