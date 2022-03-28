d0aab73a48ab32e846b3395ab394fd51d43d1e6a
billgoo <billgoo0813@gmail.com>
2022-03-28 16:17:33 +0800

    feat: update dp:labuladong/1/4

--- Related Topics/Dynamic Programming/322. Coin Change.py
+++ Related Topics/Dynamic Programming/322. Coin Change.py
@@ -2,10 +2,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [amount + 1 for _ in range(amount + 1)]
-        
+
         # base case
         dp[0] = 0
-        
+
         # status
         for i in range(amount + 1):
             # choices
@@ -13,5 +13,5 @@ class Solution:
                 if i - coin < 0:
                     continue
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-                
+
         return dp[amount] if dp[amount] != amount + 1 else -1
