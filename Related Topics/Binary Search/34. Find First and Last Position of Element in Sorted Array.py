96467b5ff532498974d1e3bd2cb07be0c4633a06
billgoo <billgoo0813@gmail.com>
2022-04-05 18:34:15 +0800

    feat: update binary search:labuladong/1/11

--- Related Topics/Binary Search/34. Find First and Last Position of Element in Sorted Array.py
+++ Related Topics/Binary Search/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,28 +1,37 @@
 # 34. Find First and Last Position of Element in Sorted Array
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l, r = 0, len(nums)
-        
-        # find left bound
-        while l < r:
-            m = (l + r) // 2
-            if nums[m] >= target:
-                r = m
+        left, right = 0, len(nums)
+        res = [-1, -1]
+
+        # find left
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                right = mid
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                l = m + 1
-        lo = l
-        
-        if lo == len(nums) or nums[lo] != target:
-            return [-1, -1]
-        
-        # find right bound
-        l, r = 0, len(nums)
-        while l < r:
-            m = (l + r) // 2
-            if nums[m] > target:
-                r = m
+                right = mid
+        if left < len(nums) and nums[left] == target:
+            res[0] = left
+        else:
+            return res
+
+        # find right
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                left = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                l = m + 1
-        
-        return [lo, l - 1]
-            
+                right = mid
+        # 这里判断右边界合法性，可以删除
+        # 因为不存在右边界时 left = 0 => res[1] = left - 1 = -1
+        # 且左边界合法，那必定存在右边界
+        # if left - 1 >= 0 and nums[left - 1] == target:
+        #     res[1] = left - 1
+
+        return res
