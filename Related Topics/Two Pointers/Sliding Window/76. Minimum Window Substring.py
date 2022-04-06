e8e7ffec55c4b7e81791f712cf304d3761607d72
billgoo <billgoo0813@gmail.com>
2022-04-06 16:26:01 +0800

    feat: update two pointer:sliding window:labuladong/1/12

--- Related Topics/Two Pointers/Sliding Window/76. Minimum Window Substring.py
+++ Related Topics/Two Pointers/Sliding Window/76. Minimum Window Substring.py
@@ -17,49 +17,49 @@
 # window substring of s such that every character in t (including duplicates)
 # is included in the window. If there is no such substring, return the empty
 # string "".
-# 
+#
 # The testcases will be generated such that the answer is unique.
-# 
+#
 # A substring is a contiguous sequence of characters within the string.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: s = "ADOBECODEBANC", t = "ABC"
 # Output: "BANC"
 # Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C'
 # from string t.
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: s = "a", t = "a"
 # Output: "a"
 # Explanation: The entire string s is the minimum window.
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: s = "a", t = "aa"
 # Output: ""
 # Explanation: Both 'a's from t must be included in the window.
 # Since the largest window of s only has one 'a', return empty string.
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # m == s.length
 # n == t.length
 # 1 <= m, n <= 10^5
 # s and t consist of uppercase and lowercase English letters.
-# 
-# 
-# 
+#
+#
+#
 # Follow up: Could you find an algorithm that runs in O(m + n) time?
 #
 
@@ -95,10 +95,9 @@ class Solution:
                 if c_ in need:
                     if window[c_] == need[c_]:
                         valid -= 1
-                    window[c_] -= 1 # 出窗口
+                    window[c_] -= 1  # 出窗口
 
         return "" if result == s + t else result
 
 
 # @lc code=end
-
